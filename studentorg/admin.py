d7be5ea6e433d5d7d8ba0464f0d430f4b04d4513
kirkgabz <202380361@psu.palawan.edu.ph>
2026-02-22 05:21:12 +0800

    added part 2

--- studentorg/admin.py
+++ studentorg/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import College, Program, Organization, Student, OrgMember
 
-# Register your models here.
+admin.site.register(College)
+class CollegeAdmin(admin.ModelAdmin):
+    list_display = ("college_name", "created_at", "updated_at")
+    search_fields = ("college_name",)
+    list_filter = ("created_at",)
+
+admin.site.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("prog_name", "college")
+    search_fields = ("prog_name", "college__college_name")
+    list_filter = ("college",)
+
+admin.site.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "college", "description")
+    search_fields = ("name", "description")
+    list_filter = ("college",)
+
+admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "lastname", "firstname", "middlename", "program")
+    search_fields = ("lastname", "firstname")
+
+admin.site.register(OrgMember)
+class OrgMemberAdmin(admin.ModelAdmin):
+    list_display = ("student", "organization", "date_joined")
+    search_fields = ("student__lastname", "student__firstname")
--- studentorg/views.py
+++ studentorg/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Organization
 
-# Create your views here.
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = 'home'
+    template_name = "home.html"
