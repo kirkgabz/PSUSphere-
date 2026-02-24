3c4aeab82fbf1aee832608e124f6fd5e4b880d9e
kirkgabz <202380361@psu.palawan.edu.ph>
2026-02-24 09:49:27 +0800

    modified admin.py

--- studentorg/admin.py
+++ studentorg/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
 from .models import College, Program, Organization, Student, OrgMember
 
-admin.site.register(College)
+@admin.register(College)
 class CollegeAdmin(admin.ModelAdmin):
     list_display = ("college_name", "created_at", "updated_at")
     search_fields = ("college_name",)
     list_filter = ("created_at",)
 
-admin.site.register(Program)
+@admin.register(Program)
 class ProgramAdmin(admin.ModelAdmin):
     list_display = ("prog_name", "college")
     search_fields = ("prog_name", "college__college_name")
     list_filter = ("college",)
 
-admin.site.register(Organization)
+@admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ("name", "college", "description")
     search_fields = ("name", "description")
