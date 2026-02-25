23a3203b0d12cdb6d14badaa95ef5662e34a06cc
kirkgabz <202380361@psu.palawan.edu.ph>
2026-02-25 15:26:43 +0800

    added Organization ListView

--- studentorg/views.py
+++ studentorg/views.py
@@ -1,7 +1,14 @@
-from django.views.generic import ListView
-from .models import Organization
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from studentorg.models import Organization
 
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
+
+class OrganizationList(ListView):
+    model = Organization
+    context_object_name = 'organization'
+    template_name = 'org_list.html'
+    paginate_by = 5
